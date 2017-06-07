4d944dc4a65910741d9fd7e1ed66c3bb9642e6c0
Alex Lapinski <contact@alexlapinski.name>
2017-06-06 21:15:58 -0400

    Added pretty echos to cleanup output to STDOUT

--- clean.sh
+++ clean.sh
@@ -8,11 +8,14 @@ CONTAINER=libsvm
 #  Used to cleanup docker artifacts
 #
 
+echo ""
 echo "Removing local artifacts"
 rm -rf ./target/
 
+echo ""
 echo "Removing Build Container: '$CONTAINER.target'"
 docker rm $CONTAINER.target
 
+echo ""
 echo "Removing Build Image: '$REPOSITORY/$CONTAINER.build'"
 docker rmi $REPOSITORY/$CONTAINER.build
--- make.sh
+++ make.sh
@@ -11,18 +11,21 @@ CONTAINER=libsvm
 #  4. Create final docker image with binaries
 #
 
-echo "Creating Build Image: 'REPOSITORY/$CONTAINER.build'"
+echo ""
+echo "Creating Build Image: '$REPOSITORY/$CONTAINER.build'"
 docker build -t $REPOSITORY/$CONTAINER.build -f Dockerfile.build .
 
+echo ""
 echo "Creating Build Container: '$CONTAINER.target'"
 docker create --name $CONTAINER.target $REPOSITORY/$CONTAINER.build
 
-mkdir ./target/
-
+echo ""
 echo "Copying artifacts from Build Container"
+mkdir ./target/
 docker cp $CONTAINER.target:/usr/src/libsvm/svm-predict ./target/
 docker cp $CONTAINER.target:/usr/src/libsvm/svm-scale ./target/
 docker cp $CONTAINER.target:/usr/src/libsvm/svm-train ./target/
 
+echo ""
 echo "Creating final image: '$REPOSITORY/$CONTAINER'"
 docker build -t $REPOSITORY/$CONTAINER .
