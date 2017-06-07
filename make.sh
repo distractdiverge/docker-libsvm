f27fd4bebb5ceedc703cbb6a070fc7fa9a53a5bb
Alex Lapinski <contact@alexlapinski.name>
2017-06-06 21:10:14 -0400

    Added cleanup tasks

--- make.sh
+++ make.sh
@@ -11,13 +11,24 @@ CONTAINER=libsvm
 #  4. Create final docker image with binaries
 #
 
+echo "Creating Build Image: 'REPOSITORY/$CONTAINER.build'"
 docker build -t $REPOSITORY/$CONTAINER.build -f Dockerfile.build .
+
+echo "Creating Build Container: '$CONTAINER.target'"
 docker create --name $CONTAINER.target $REPOSITORY/$CONTAINER.build
 
 mkdir ./target/
 
+echo "Copying artifacts from Build Container"
 docker cp $CONTAINER.target:/usr/src/libsvm/svm-predict ./target/
 docker cp $CONTAINER.target:/usr/src/libsvm/svm-scale ./target/
 docker cp $CONTAINER.target:/usr/src/libsvm/svm-train ./target/
 
+echo "Removing Build Container: '$CONTAINER.target'"
+docker rm $CONTAINER.target
+
+echo "Removing Build Image: '$REPOSITORY/$CONTAINER.build'"
+docker rmi $REPOSITORY/$CONTAINER.build
+
+echo "Creating final image: '$REPOSITORY/$CONTAINER'"
 docker build -t $REPOSITORY/$CONTAINER .
